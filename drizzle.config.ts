c857b8a9f61fdf2c53ce27ea92507e07c593fb62
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-11-26 09:10:25 +0700

    chore: added .env.example

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,18 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+// `serial` data type currently is not supported by Amazon Aurora DSQL
+// so we cannot run the migrations using `drizzle-kit migrate`
 export default defineConfig({
-    out: "./db/drizzle",
-    schema: "./db/schema.ts",
-    dialect: "postgresql",
-    dbCredentials: {
-        // url: process.env.DATABASE_URL!,
-        host: process.env.DB_HOST!,
-        user: process.env.DB_USER!,
-        database: process.env.DB_DATABASE!,
-        password: process.env.DB_PASSWORD,
-        ssl: true,
-    },
+  out: "./db/drizzle",
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    // url: process.env.DATABASE_URL!,
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    database: process.env.DB_DATABASE!,
+    password: process.env.DB_PASSWORD,
+    ssl: true,
+  },
 });
