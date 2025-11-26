3aba8baac2add5b6aaed4b1882e8f6d86082ea89
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-11-26 09:20:17 +0700

    fix: increased the expires time for token

--- db/auth.ts
+++ db/auth.ts
@@ -1,16 +1,17 @@
 import { DsqlSigner } from "@aws-sdk/dsql-signer";
 
 export async function generateToken() {
-    const signer = new DsqlSigner({
-        hostname: process.env.DB_HOST!,
-    });
-    try {
-        // Use `getDbConnectAuthToken` if you are _not_ logging in as the `admin` user
-        const token = await signer.getDbConnectAdminAuthToken();
-        console.log(token);
-        return token;
-    } catch (error) {
-        console.error("Failed to generate token: ", error);
-        throw error;
-    }
+  const signer = new DsqlSigner({
+    hostname: process.env.DB_HOST!,
+    expiresIn: 604800,
+  });
+  try {
+    // Use `getDbConnectAuthToken` if you are _not_ logging in as the `admin` user
+    const token = await signer.getDbConnectAdminAuthToken();
+    console.log(token);
+    return token;
+  } catch (error) {
+    console.error("Failed to generate token: ", error);
+    throw error;
+  }
 }
